306e9ea8467a0c992e18455afcc0d26c68363531
DomDL <domix.deluca@gmail.com>
2019-11-17 23:11:14 +0100

    Riscritta la funzione che assegna i punti

--- src/control.c
+++ src/control.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 #include <curses.h>
+#include <string.h>
 
 #include "control.h"
 #include "utils.h"
 
 
-void score_handler(int * score, Entity tmp_pkg);
+void food_handler(int * score, Position pos, int rows, int col, char game_food[rows][col]);
+void food_setup();
 
 void control_main(int pos_in, int ghost_out)
 {
@@ -16,13 +18,17 @@ void control_main(int pos_in, int ghost_out)
     int i,j;
 
     int score = 0;
-    char next_ch;
+    char game_food[MAP_HEIGHT][MAP_WIDTH];
+    int eated_dots; //Dopo 70 e 170 spawna frutto
+
+    food_setup(MAP_HEIGHT, MAP_WIDTH, game_food);
+
     while(1)
     {
         mvprintw(2, 60, "Score:   %d", score);
         read(pos_in, &tmp_pkg, sizeof(tmp_pkg));    //leggo posizione di pacman
         
-        score_handler(&score, tmp_pkg);
+        food_handler(&score, pacman.p, MAP_HEIGHT, MAP_WIDTH, game_food);
 
         for(i=-1; i<=1/* code */; i++)    //cancella pacman
         {
@@ -41,10 +47,20 @@ void control_main(int pos_in, int ghost_out)
     }
 }
 
-void score_handler(int * score, Entity tmp_pkg)
+void food_setup(int row, int col, char game_food[row][col])
 {
+    int i;
+    for (i = 0; i < MAP_HEIGHT; i++)
+    {
+        strcpy(game_food[i], PELLETS[i]);
+        mvprintw(9, 60, "Debug:   %d", i);
+    } 
+}
+
+void food_handler(int * score, Position pos,int rows, int col, char game_food[rows][col])
+{   //Diventerà uno switch
     char next_ch;
-    switch (tmp_pkg.dir)
+    /*switch (tmp_pkg.dir)
         {
             case UP:
                 next_ch = mvinch(tmp_pkg.p.y, tmp_pkg.p.x); //prende carattere a schermo nella pos y,x
@@ -66,5 +82,15 @@ void score_handler(int * score, Entity tmp_pkg)
                 if(next_ch == '~')
                     *score += 100;
             break;
-        }
+        }*/
+    if(game_food[pos.y][pos.x] == '~')
+    {
+        *score += 10;
+        game_food[pos.y][pos.x] = MAP[pos.y][pos.x];
+    }
+    else if(game_food[pos.y][pos.x] == '~') //energizers
+    {
+        *score += 50;
+        //Funzione "spaventa fantasmi"
+    }
 }
