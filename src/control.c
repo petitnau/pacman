5f417f3348923e17b8302947cecb618e2954c831
petitnau <roberto.pettinau99@gmail.com>
2019-11-18 01:08:23 +0100

    Fix contatore cibo e cibo non ristampato

--- src/control.c
+++ src/control.c
@@ -25,23 +25,23 @@ void control_main(int pos_in, int ghost_out)
 
     while(1)
     {
-        mvprintw(2, 60, "Score:   %d", score);
         read(pos_in, &tmp_pkg, sizeof(tmp_pkg));    //leggo posizione di pacman
         
-        food_handler(&score, pacman.p, MAP_HEIGHT, MAP_WIDTH, game_food);
-
         for(i=-1; i<=1/* code */; i++)    //cancella pacman
         {
             pos.x=pacman.p.x+i;
             pos.y=pacman.p.y;
             pos = get_pac_eff_pos(pos);
             
-            mvaddch(pos.y, pos.x, MAP[pos.y][pos.x]);
+            mvaddch(pos.y, pos.x, NCURSES_ACS(game_food[pos.y][pos.x]));
         }
         
         pacman = tmp_pkg;
         
-        mvprintw(0,60, "x:%d,y:%d,d:%d", pacman.p.x, pacman.p.y, pacman.dir);
+        food_handler(&score, pacman.p, MAP_HEIGHT, MAP_WIDTH, game_food);
+
+        mvprintw(0, 60, "x:%.1f,y:%d,d:%d", pacman.p.x/2.0, pacman.p.y, pacman.dir);
+        mvprintw(2, 60, "Score:   %d", score);
         print_pacman(pacman);
         refresh();
     }
@@ -57,41 +57,27 @@ void food_setup(int row, int col, char game_food[row][col])
     } 
 }
 
-void food_handler(int * score, Position pos,int rows, int col, char game_food[rows][col])
-{   //Diventerà uno switch
-    char next_ch;
-    /*switch (tmp_pkg.dir)
+void food_handler(int* score, Position pos, int rows, int col, char game_food[rows][col])
+{  
+    int i;
+    Position pe_pos;
+
+    for(i=-1; i<=1; i++)
+    {
+        pe_pos.x = pos.x+i;
+        pe_pos.y = pos.y;
+        pe_pos = get_pac_eff_pos(pe_pos);
+
+        switch(game_food[pe_pos.y][pe_pos.x])
         {
-            case UP:
-                next_ch = mvinch(tmp_pkg.p.y, tmp_pkg.p.x); //prende carattere a schermo nella pos y,x
-                if(next_ch == '~')
-                    *score += 100;
+            case '~':
+                *score += 10;
                 break;
-            case DOWN:
-                next_ch = mvinch(tmp_pkg.p.y, tmp_pkg.p.x);
-                if(next_ch == '~')
-                    *score += 100;
-            break;
-            case RIGHT:
-                next_ch = mvinch(tmp_pkg.p.y, tmp_pkg.p.x+1);
-                if(next_ch == '~')
-                    *score += 100;
-            break;
-            case LEFT:
-                next_ch = mvinch(tmp_pkg.p.y, tmp_pkg.p.x-1);
-                if(next_ch == '~')
-                    *score += 100;
-            break;
-        }*/
-    if(game_food[pos.y][pos.x] == '~')
-    {
-        *score += 10;
-        game_food[pos.y][pos.x] = MAP[pos.y][pos.x];
-    }
-    else if(game_food[pos.y][pos.x] == '`') //energizers
-    {
-        *score += 50;
-        game_food[pos.y][pos.x] = MAP[pos.y][pos.x];
-        //Funzione "spaventa fantasmi"
+            case '`': 
+                //Funzione "spaventa fantasmi"
+                *score += 50;
+                break;
+        }
+        game_food[pe_pos.y][pe_pos.x] = ' ';
     }
 }
